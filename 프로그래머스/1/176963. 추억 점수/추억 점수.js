33cef35c70686aad26a709c44edad0e1a7eba4a4
VUE_LJS <45707242+IGam94@users.noreply.github.com>
2023-10-19 11:18:07 +0900

    [level 1] Title: 추억 점수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,16 +1,13 @@
 function solution(name, yearning, photo) {
     let result = {}
     name.map((key, i) => (result[key]= yearning[i]));
-    console.log(result)
     let score = photo.map((key,i)=>{
         let plus = 0
         key.map((item)=>{
-            console.log('타입',typeof result[item])
             typeof result[item]==='number'? plus += result[item] : ''
         })
         return plus
     })
     
-    var answer = [];
     return score;
 }
